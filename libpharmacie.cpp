3847ee3e80578beec49e26911a38eafdfbccbddb
jtombiamba <james.tombi@gmail.com>
2016-06-21 15:42:48 +0200

    new commit

--- libpharmacie.cpp
+++ libpharmacie.cpp
@@ -1,7 +1,12 @@
 #include <cstdio>
 #include <cstring>
+<<<<<<< HEAD
 
 
+=======
+#include <vector>
+#include <algorithm>
+>>>>>>> ursuleBranch
 int test(int a, char * b)
 {
 
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+
+int main(int argc, char ** argv)
+{
+
+  cout << "hello, it's me!\n";
+  cout << "an example\n";
+  return 0;
+}
