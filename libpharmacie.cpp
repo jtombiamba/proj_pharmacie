949fe0232fa295ac46478e865d94750c71973f6d
jtombiamba <james.tombi@gmail.com>
2016-06-21 15:26:37 +0200

    Revert "Merge branch 'ursuleBranch'"
    This reverts commit fd81a58217de189906a6897a8126d3ea14a19a8c, reversing
    changes made to 764a03b706dd737fce2c7227cd9e9de17b9a53c2.

--- libpharmacie.cpp
+++ libpharmacie.cpp
@@ -1,16 +1,9 @@
-#include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <vector>
+
 
 int test(int a, char * b)
 {
 
   return 0;
-}
-
-void retest(float *b, vector<int> S, float *Z)
-{
-
-  return ;
 }
