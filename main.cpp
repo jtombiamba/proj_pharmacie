461c3098960d125ad96db55ac603a7c8a83c49c7
jtombiamba <james.tombi@gmail.com>
2016-07-01 16:00:38 +0200

    insertion complete des effets indesisrables

--- main.cpp
+++ main.cpp
@@ -16,12 +16,15 @@ int main(int argc, char ** argv)
   int b = 0;
   int c(0);
   int med(0);
-  int ind(0);
+  int ind(0), ind2(0);
   string in_line;
+
   unordered_map <string, vector<string>> Biblio;
   std::unordered_map <string, vector<string>>::iterator it;
   std::vector<string>::iterator it2;
+
   ifstream file(argv[1]);
+
   cout << "hello, it's me!" << endl;
 
   // ***********Read lines and parse from database.txt
@@ -29,24 +32,45 @@ int main(int argc, char ** argv)
     {
       a = in_line.find("effets indÃ©sirables notoires",0);
       in_line = in_line.erase(a-1, 33);
+      in_line = in_line.erase(in_line.size()-1,1);
       c = in_line.find(" et ", 0);
       if (c != -1) 
 	in_line = in_line.replace(c, 4, ", ", 2);
       
       med = in_line.find(" :: ", 0);
       ind = in_line.find(", ", med+4);
-      //cout << in_line.substr(med+4,ind-med-4) << endl;
+
+      //start one insertion in hashmap
       if (c == -1)
       	{
 	
-      	  Biblio[in_line.substr(0, med)] = vector <string> (1, in_line.substr(med+4,in_line.size() - med - 5));
+      	  Biblio[in_line.substr(0, med)] = vector <string> (1, in_line.substr(med+4,in_line.size() - med - 4));
       	}
+      //start multiple insertions in hashmap
       else
       	{
       	  Biblio[in_line.substr(0, med)] = vector <string> (1, in_line.substr(med+4,ind-med-4));
+
+	  //find the new entry in the map 
+	  it = Biblio.find(in_line.substr(0, med));
+ 
+	  //cout << in_line.substr(ind2,ind-ind2) << endl;
+	 
+	  if(it!=Biblio.end())
+	    { 
+	      //add element in the vector while there are indesirable effects
+	      while(ind!=-1)
+		{
+		  ind2 = ind + 2;
+		  ind = in_line.find(", ", ind2);
+
+		  it2 = it->second.end();
+		  it->second.insert(it2, in_line.substr(ind2,ind-ind2));
+		}
+	    }
       	}
 
-      cout << " ligne " << ++b << " : " << in_line << ", " << in_line.substr(0, med) << " " << endl;
+      cout << " ligne " << ++b << " : " << in_line << " " << endl;
       //with all that done, we will find every ::, the string between pos 0 and :: will be the drug
       //after storing the drug, we cut the string
       //between every column, we will find an undesirable effect by taking the substring between the consecutive positions
@@ -55,14 +79,15 @@ int main(int argc, char ** argv)
       //the map will be declared unordered_map <string, vector<string>> (check in internet)
     }
   cout << endl;
+
   for(it=Biblio.begin();it!=Biblio.end();++it)
     {
       cout << it->first << " ==> ";
       for (it2=it->second.begin();it2!=it->second.end();++it2)
 	{
-	  cout << *it2 ;
+	  cout << *it2 << ", " ;
 	}
-      cout << endl;
+      cout << it->second.size() << endl;
       //cout << it->first << " ==> " << it->second << endl;
     }
   return 0;
